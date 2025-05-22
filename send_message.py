da388d093b90e23ec8535150ef0af5b3f84b79f7
777den7771 <777den1996@gmail.com>
2025-05-22 00:34:21 -0500

    Delete send_message.py

--- send_message.py
+++
@@ -1,6 +0,0 @@
-from telegram import Bot
-from config import TELEGRAM_TOKEN, CHAT_ID
-
-bot = Bot(token=TELEGRAM_TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="👋 Привет! Это бот.")
-print("Сообщение отправлено.")
