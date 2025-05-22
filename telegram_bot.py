76b776c48ead07a16eaa448a18f588066efbdd4d
777den7771 <777den1996@gmail.com>
2025-05-22 00:59:22 -0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,6 +6,6 @@ bot = Bot(token=7764776672:AAG77dSGvj9GQAQ65qTeablUymI3M0NdWPE)
 def send_message(text):
     try:
         print("Отправляю сообщение в Telegram...")
-        bot.send_message(chat_id=CHAT_ID, text=text)
+        bot.send_message(chat_id=6086012346, text=text)
     except Exception as e:
         print(f"[Ошибка при отправке сообщения]: {e}")
